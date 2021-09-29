862b3fe42b6954f67cbd839cc56f40c522e3c33e
trongcauta <trongcauhcmus@gmail.com>
2021-09-29 16:47:20 +0700

    update therocks token

--- contracts/TradeToken.sol
+++ contracts/TradeToken.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+/**
+ * this contract for testing only
+ */
 contract MyToken is ERC20, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 1000000000 * 10**9);
